e92b0ee45480e3a4d62e15a6ac081d2edecc97c5
朱骏杰 <ories.zhu@mochain.vip>
2020-06-27 12:58:18 +0800

    add typescript hello world

--- app2.ts
+++ app2.ts
@@ -6,7 +6,11 @@ app.get('/', function (request, response) {
   response.send('你好');
 });
 
+
+app.post('./',(request, response, next)=>{})
+
 const port = 4000;
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}!`);
 });
+
