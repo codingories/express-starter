cbd6e0428c272395d2e6932a6eec249ece4d285f
朱骏杰 <ories.zhu@mochain.vip>
2020-06-27 17:05:46 +0800

    路由语法糖

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
 const express = require('express')
-const logger =require('./logger')
+const logger = require('./logger')
 const app = express()
-
 
 
 app.use(logger('dev'))
 
-app.use((request, response, next)=>{
-  if (request.path === '/' && request.method === "get" ){
+app.use((request, response, next) => {
+  if (request.path === '/' && request.method === "get") {
     response.send('根目录')
   }
   next()
 })
 
-app.get('/xxx',(request, response, next)=>{
+app.get('/xxx', (request, response, next) => {
   response.send('xxx 页面')
   next()
 })
+
+app.route('/xxxx')
+  // .all(() => {console.log('ffff')})
+  .get((request, response, next) => {
+    console.log('ttttta')
+    response.send('xxxx')
+    response.end()
+    next()
+  })
 
 
 app.listen(3000, () => {
