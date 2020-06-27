0de39276f401d5636d2628e2495e3408549304f4
朱骏杰 <ories.zhu@mochain.vip>
2020-06-27 16:33:04 +0800

    自己实现可以传入参数的中间件logger,作用打印每次请求的路径

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 const express = require('express')
-
+const logger =require('./logger')
 const app = express()
 
+
+
+app.use(logger('dev'))
+
 app.use((request, response, next) => {
-  console.log(request.url)
   response.write('hi')
   next() // next表示当前的功能已经完成了，进入下一个功能,send只能发一次
 })
 
 app.use((request, response, next) => {
-  console.log(2)
   response.write('hello')
   next()
 })
 
 app.use((request, response, next) => {
-  console.log(3)
   response.end()
   next()
 })
